0f4cd3232627dc68d4f5f78f0b56de87d6d4b366
Megan Comeau <mlcomeau99@gmail.com>
2020-04-30 18:56:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,11 @@
 require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   html= File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+  binding.pry 
 end
+
+# projects: kickstarter.css("li.project.grid_4")
